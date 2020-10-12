047f4e08535014c7401fa8093622c5b47425c0d2
Lawand Othman <lwnd@pm.me>
2020-10-13 00:22:58 +0100

    change theme

--- litter-client/src/App.js
+++ litter-client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
+import MuiThemeProvide from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 // Components
 import Navbar from './components/Navbar'
@@ -10,20 +12,35 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#212121',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      main: '#FAFAFA',
+      contrastText: '#212121',
+    },
+  },
+})
+
 function App() {
   return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home}></Route>
-            <Route exact path='/login' component={Login}></Route>
-            <Route exact path='/signup' component={Signup}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvide theme={theme}>
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home}></Route>
+              <Route exact path='/login' component={Login}></Route>
+              <Route exact path='/signup' component={Signup}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvide>
   )
 }
 
